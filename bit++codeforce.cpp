10c2465f8be53e3cf28ec3d683f2debe0d56f3ee
prashant kumar <kumarprashant9972@gmail.com>
2021-03-15 14:38:50 +0530

    solution

---
+++ bit++codeforce.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,x=0 ;
+	cin>>n;
+	while(n--){
+		string s;
+		cin>>s;
+		if(s=="++X"||s=="X++")
+		x +=1;
+		if(s=="--X"||s=="X--")
+		x -=1;
+	}
+	cout<<x<<endl;
+}
